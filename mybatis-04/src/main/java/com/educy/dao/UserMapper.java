8c078109473301627b1afe6c5d12998185d02a32
jingxian99 <2439644676@qq.com>
2020-10-04 09:24:28 +0800

    Limit以及RowBounds分页查询 pagehelper分页查询插件了解即可 https://pagehelper.github.io/docs/howtouse/

--- mybatis-04/src/main/java/com/educy/dao/UserMapper.java
+++ mybatis-04/src/main/java/com/educy/dao/UserMapper.java
@@ -2,6 +2,9 @@ package com.educy.dao;
 
 import com.educy.entity.User;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * @Author 马小姐
  * @Date 2020-09-28 14:52
@@ -14,4 +17,7 @@ public interface UserMapper {
 //    @Select("select * from user where id= 41")
      User getUserById(int i);
 
+     List<User> getUserByLimit(Map<String,Integer> map);
+
+     List<User> getUserByRowBounds();
 }
--- mybatis-04/src/test/java/com/educy/dao/UserDaoTest.java
+++ mybatis-04/src/test/java/com/educy/dao/UserDaoTest.java
@@ -1,11 +1,15 @@
 package com.educy.dao;
 
+import com.educy.entity.User;
 import com.educy.entity.tUser;
 import com.educy.util.MybatisUtils;
+import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.List;
 
 
 /**
@@ -44,6 +48,36 @@ public class UserDaoTest {
         logger.debug("debug:进入了debug");
         logger.error("error:");
 
+    }
+
+
+    //实现分页查询
+    @Test
+    public void getUserByLimit() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        HashMap<String, Integer> stringIntegerHashMap = new HashMap<String, Integer>();
+        stringIntegerHashMap.put("startIndex",0);
+        stringIntegerHashMap.put("endIndex",7);
+        List<User> userByLimit = mapper.getUserByLimit(stringIntegerHashMap);
+        for (User user : userByLimit) {
+            System.out.println(user);
+        }
+        sqlSession.close();
+    }
+
+    //是以RowBounds实现分页查询
+    @Test
+    public void getUserByRowBounds(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        RowBounds rowBounds = new RowBounds(2,10);
+
+        List<User> userList = sqlSession.selectList("com.educy.dao.UserMapper.getUserByRowBounds", null, rowBounds);
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+        sqlSession.close();
     }
 
 }
